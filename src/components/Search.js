ce85d7171c7b5573d6580efa39a06b5063619563
andreafrank <andreajfrank23@gmail.com>
2021-03-05 21:16:44 -0600

    successfully implement search with nasa images-api, returns all images, unformatted

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,53 +2,76 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Search = async () => {
-	const [term, setTerm] = useState('jupiter');
+const Search = () => {
+	const [term, setTerm] = useState('green');
   const [results, setResults] = useState([]);
 
-  const api_call = await fetch(
-    // does not seem to accept api key
-    `https://images-api.nasa.gov/search?q=magenta&media_type=image`
-  );
-  const data = await api_call.json();
-      const dataInfo = data.collection.items[0]
-      const gettingCloser = dataInfo.links[0]
-      console.log(gettingCloser.href)
+  useEffect(() => {
+    const search = async () => {
+      const data = await axios.get('https://images-api.nasa.gov/search?', {
+        params: {
+          q: `${term}`,
+          media_type: 'image',
+        },
+      })
+			.then((response) => {
+				setResults(response.data.collection.items)
+				console.log(results)
+			})
+			// todo: set up .catch
+    };
 
-      // return (
-      //   data.collection
-      // )
+		console.log(results)
+    if (term && !results.length) {
+			 search();
+		} else {
+			const timeoutId = setTimeout(() => {
+				if (term) {
+					search();
+				};
+			}, 500);
 
-  return (
-    <div>
-      <div>
-        <label>search for space stuff!</label>
-        <input
-          value={term}
-          onChange= {e => setTerm(e.target.value)}
-        />
-        </div>
-    </div>
+			return () => {
+				clearTimeout(timeoutId);
+			};
+		}
+	}, [term]);
 
-  )
+  const renderedResults = results.map((result) => {
+		const nasaId = (result.data[0].nasa_id);
+		const nasaHref = (result.links[0].href);
+		const nasaDescription = (result.data[0].description);
 
-	// onFormSubmit = (e) => {
-	// 	e.preventDefault();
-	// 	this.props.onSubmit(this.state.term)
-	// };
-  //
-	// render() {
-	// 	return (
-	// 		<div className="ui segment">
-	// 			<form onSubmit={this.onFormSubmit} className="ui form">
-	// 				<div className="field">
-	// 					<label>image search</label>
-	// 					<input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
-	// 				</div>
-	// 			</form>
-	// 		</div>
-	// 	);
-	// };
+		return (
+			<div key={nasaId}>
+				<div>
+					<img src={nasaHref}
+						alt={nasaDescription}
+					/>
+				</div>
+			</div>
+		);
+	});
+
+	return (
+		<div>
+			<div>
+				<div>
+					<label>Enter search term</label>
+					<input
+						value={term}
+						onChange={e => setTerm(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div>
+				found NUMBER results
+				{renderedResults}
+			</div>
+		</div>
+	)
 };
 
 export default Search;
+
+// href={`https://images-assets.nasa.gov/${nasaId}/${nasaId}~thumb.jpg`}
